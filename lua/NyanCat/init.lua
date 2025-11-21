eeecef247ed32fcf0f35a16d8277cadf2062c75b
larry <segfaulted67@gmail.com>
2025-11-21 16:46:40 +0530

    fix: removed emoji and added ':>'

--- lua/NyanCat/init.lua
+++ lua/NyanCat/init.lua
@@ -1,21 +1,22 @@
 -- nyan.lua
 local M = {}
 
-M.config = {
+M.opts = {
     width = 30,
-    cat = { "🐱" },
+    cat = { ":>" },
     trail = "━",
-    colors = { "Red", "Orange", "Yellow", "Green", "Blue", "Purple" },
-    rainbow = {
-        "#BF1119", "#F52A02", "#FC7800", "#FBA500",
-        "#F0D300", "#B4BF00", "#4D9801", "#1E7FF7",
-    }
+    
+    colors = {"Red", "Orange", "Yellow", "Green", "Blue", "Purple"},
 }
+
+function M.setup(opts)
+    M.opts = vim.tbl_deep_extend("force", M.opts, opts or {})
+end
 
 function M.render()
     local total = vim.fn.line("$")
     local current = vim.fn.line(".")
-    local width = M.config.width
+    local width = M.opts.width
     local pos = math.ceil(current / total * width)
     local percentage = (total > 0) and math.floor(100 * current / total) or 0
     if pos < 1 then pos = 1 end
@@ -25,11 +26,11 @@ function M.render()
     table.insert(segments, ("L%s "):format(current))
     for i = 1, width do
         if i == pos then
-            local frame = (current % #M.config.cat) + 1
-            table.insert(segments, M.config.cat[frame])
+            local frame = (current % #M.opts.cat) + 1
+            table.insert(segments, M.opts.cat[frame])
         elseif i <= pos then
-            local color = M.config.colors[(i-1) % #M.config.colors + 1]
-            table.insert(segments, ("%s"):format(M.config.trail))
+            local color = M.opts.colors[(i-1) % #M.opts.colors + 1]
+            table.insert(segments, ("%s"):format(M.opts.trail))
         else
           table.insert(segments, (" "))
         end
